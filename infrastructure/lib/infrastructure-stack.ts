172491fadd78a54ce4fbbcd4ad5c102cf9da2e74
Roddy Tomlin <roddytdog@yahoo.com>
2020-07-11 16:18:58 -0700

    OAI <> Bucket

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as s3 from '@aws-cdk/aws-s3';
-import * as iam from '@aws-cdk/aws-iam';
+//import * as iam from '@aws-cdk/aws-iam';
+import * as cloudfront from '@aws-cdk/aws-cloudfront';
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -9,17 +10,34 @@ export class InfrastructureStack extends cdk.Stack {
     // The code that defines your stack goes here
     const bucket = new s3.Bucket(this, 'WebsiteBucket', {
       bucketName: 'wellcall-app-cdk',
-      websiteIndexDocument: 'index.html',
-      blockPublicAccess: new s3.BlockPublicAccess({restrictPublicBuckets: false})
+      websiteIndexDocument: 'index.html'
     });
 
-    const bucketPolicy = new iam.PolicyStatement({
-      actions: ['s3:GetObject'],
-      resources: [
-        `${bucket.bucketArn}/*`
-      ],
-      principals: [new iam.Anyone()],
+    // USE WHEN SERVING FROM BUCKET
+    // const bucketPolicy = new iam.PolicyStatement({
+    //   actions: ['s3:GetObject'],
+    //   resources: [
+    //     `${bucket.bucketArn}/*`
+    //   ],
+    //   principals: [new iam.Anyone()],
+    // })
+    // bucket.addToResourcePolicy(bucketPolicy);
+
+    // USE WHEN SERVING FROM OAI
+    const cloudFrontOAI = new cloudfront.OriginAccessIdentity(this, 'OAI');
+
+    const distribution = new cloudfront.CloudFrontWebDistribution(this, 'MyDistribution', {
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: bucket,
+            originAccessIdentity: cloudFrontOAI,
+          },
+          behaviors: [{ isDefaultBehavior: true }]
+        }
+      ]
     })
-    bucket.addToResourcePolicy(bucketPolicy);
+    
+    bucket.grantRead(cloudFrontOAI.grantPrincipal);
   }
 }
