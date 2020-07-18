89f1129ce1bda812126a6907f28dc55b174dbcd4
Roddy Tomlin <roddytdog@yahoo.com>
2020-07-18 13:17:51 -0700

    unused

--- client/src/well-call/nav/Nav.tsx
+++ client/src/well-call/nav/Nav.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router,
   Switch,
-  Route,
-  Link,
-  NavLink
+  Route
 } from 'react-router-dom';
 import { Menu } from 'antd';
 import { HomeOutlined, MailOutlined, ReadOutlined } from '@ant-design/icons';
