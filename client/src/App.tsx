162cd27590a45500462dfe8772bb37f754388a9c
Roddy Tomlin <roddytdog@yahoo.com>
2020-07-18 12:50:24 -0700

    antd installed. nav in place.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,52 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-import About from './well-call/about/About';
-import Contact from './well-call/contact/Contact';
-import Home from './well-call/home/Home';
+import Nav from './well-call/nav/Nav';
 
 function App() {
   // application logic begins here
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //   </header>
-    // </div>
-    <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
+        <Nav />
       </div>
-    </Router>
   );
 }
 
---
+++ client/src/well-call/nav/Nav.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink
+} from 'react-router-dom';
+import { Menu } from 'antd';
+import { HomeOutlined, MailOutlined, ReadOutlined } from '@ant-design/icons';
+import './Nav.css';
+import About from '../about/About';
+import Contact from '../contact/Contact';
+import Home from '../home/Home';
+
+const { SubMenu } = Menu;
+
+interface NavProps {
+}
+
+export default function Nav(props: NavProps) {
+
+const handleClick = (e: Event) => {
+    console.log('Clicked')
+}
+
+    return (
+        <div className={'nav'}>
+
+            <Router>
+                <Menu
+                    onClick={() => handleClick}
+                    mode={'horizontal'}>
+                    <Menu.Item 
+                        key={'Home'}
+                        icon={<HomeOutlined />}>
+                        <a href={'/'}>Home</a>
+                    </Menu.Item>
+                    <Menu.Item 
+                        key={'about'}
+                        icon={<ReadOutlined />}>
+                        <a href={'/about'}>About</a>
+                    </Menu.Item>
+                    <Menu.Item 
+                        key={'Contact'}
+                        icon={<MailOutlined />}>
+                        <a href={'/contact'}>Contact</a>
+                    </Menu.Item>
+                </Menu>
+
+                <Switch>
+                <Route path="/about">
+                    <About />
+                </Route>
+                <Route path="/contact">
+                    <Contact />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+                </Switch>
+            </Router>
+        </div>
+    )
+}
